a653d1f0ca3f10ce58d24ec3705822d87db8d15c
Jared Harbison <jared.harbison@gmail.com>
2019-02-27 18:06:26 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,11 +1,11 @@
 class Puppy
   attr_reader :breed
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :breed
 
   def initialize(breed, name, age)
     @breed = breed
     @name = name
     @age = age
-  end 
+  end
 
 end
